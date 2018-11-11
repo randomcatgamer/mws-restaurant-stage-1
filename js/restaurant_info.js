ad5fd5b0e612b85732cd74ddb3ee94ccee09e340
Randomcatgamer <randomcatgamer@gmail.com>
2018-11-11 21:04:00 +0100

    Made it responsive

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'sk.eyJ1IjoicmFuZG9tY2F0Z2FtZXIiLCJhIjoiY2pvY3Y0YjRsMXpjZTNrb2RraXNxd2FkeiJ9.o3DpqMc77e7xhHLRf38gXA',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
